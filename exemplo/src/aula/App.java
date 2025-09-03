66721cf48108ce2f3ef936a5d81bb610cf5ecdae
Kamily Matias <kamily.sonntag.matias@escola.pr.gov.br>
2025-09-03 11:30:38 -0300

    hghyu

--- exemplo/src/aula/App.java
+++ exemplo/src/aula/App.java
@@ -1,13 +1,14 @@
 package aula;
 
 public class App {
-    public static void main(String[] args) {
-        int a;
-        String b;
-
-        a=14;
-        b= (a>15)? "atrasado" : (a<15)? "adiantado" : "no horário";
-        System.out.println("resultado " + b);
-        
+   
+    public class IncrementoFor {
+        public static void main(String[] args) {
+            int x = 0;
+    
+            for (; x < 10; x++) {
+                System.out.println("x = " + (x + 1));
+            }
+        }
     }
 }
